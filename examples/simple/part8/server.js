a819c14d8d70e997ee35b5124eb413b4a78e42d5
Drew Folta <drew@folta.net>
2012-05-26 17:35:42 -0700

    pulling in missing example application

---
+++ examples/simple/part8/server.js
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2011-2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
+module.exports = require('mojito').createServer({dir:__dirname});
