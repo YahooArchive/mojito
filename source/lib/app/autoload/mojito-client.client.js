09d91dbe058bb384cd2f67463e9c72ac63ae201d
add0n <bedney@yahoo-inc.com>
2012-07-26 17:13:52 -0700

    [Fix 5712477] Make sure to callback when there is an error

--- source/lib/app/autoload/mojito-client.client.js
+++ source/lib/app/autoload/mojito-client.client.js
@@ -678,6 +678,15 @@ YUI.add('mojito-client', function(Y, NAME) {
             this.resourceStore.expandInstanceForEnv('client',
                 command.instance, this.context, function(err, details) {
 
+                    if (err) {
+                        if (typeof cb === 'function') {
+                            cb(new Error(err));
+                            return;
+                        } else {
+                            throw new Error(err);
+                        }
+                    }
+
                     // if there is a controller in the client type details, that
                     // means the controller exists here "cast details.controller
                     // to Boolean" ;)
