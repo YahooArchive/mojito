3eb7a73f693ce52878a7352888054bf75859d145
Drew Folta <drew@folta.net>
2012-08-01 10:07:09 -0700

    added unit test

--- source/lib/tests/autoload/app/autoload/controller-context-tests.common.js
+++ source/lib/tests/autoload/app/autoload/controller-context-tests.common.js
@@ -4,22 +4,15 @@
  * See the accompanying LICENSE file for terms.
  */
 YUI.add('mojito-controller-context-tests', function(Y, NAME) {
-    
+
     var suite = new YUITest.TestSuite(NAME),
         A = YUITest.Assert,
         OA = YUITest.ObjectAssert;
-    
+
     suite.add(new YUITest.TestCase({
-        
+
         name: 'instantiation',
 
-        setUp: function() {
-
-        },
-
-        tearDown: function() {
-
-        },
 
         'TODO: controller context caches controller instance between actions': function() {
 //            var mockInstance = {};
@@ -37,10 +30,34 @@ YUI.add('mojito-controller-context-tests', function(Y, NAME) {
 //
 //            mockYUIInst.verify();
             A.skip();
+        },
+
+
+        'support Y.Model': function() {
+            var options,
+                context;
+            options = {
+                Y: {
+                    mojito: {
+                        controller: {},
+                        models: {
+                            foo: function() {}
+                        }
+                    }
+                },
+                instance: {},
+                dispatch: function() {},
+                store: {
+                    getAppConfig: function() { return {}; }
+                }
+            };
+            context = new Y.mojito.ControllerContext(options);
+            A.areSame(options.Y.mojito.models.foo, context.models.foo);
         }
 
+
     }));
-    
+
     YUITest.TestRunner.add(suite);
-    
+
 }, '0.1.0', {requires: ['mojito-controller-context']});
