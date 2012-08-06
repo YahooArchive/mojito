b74b7a702d2b07f9176532940b62e35136a86e6b
Michael Ridgway <mridgway@yahoo-inc.com>
2012-08-06 16:11:40 -0700

    Issue #270: Added unit tests for client and server handlebars engines

--- source/lib/app/addons/view-engines/hb.client.js
+++ source/lib/app/addons/view-engines/hb.client.js
@@ -16,39 +16,6 @@ YUI.add('mojito-hb', function(Y, NAME) {
         cache = Y.namespace('Env.Handlebars');
 
     /**
-     * Cache the reference to a compiled handlebar template, plus
-     * a raw string representation of the template.
-     * @param {string} tmpl The name of the template to render.
-     * @param {boolean} bypassCache Whether or not we should rely on the cached content.
-     * @param {function} callback The function that is called with the compiled template
-     * @return {object} literal object with the "raw" and "template" references.
-     */
-    function getTemplateObj(tmpl, bypassCache, callback) {
-        var handler = function (id, resp) {
-                var str = resp.responseText;
-                if (str) {
-                    // applying a very simple local cache to avoid reading from disk over and over again.
-                    // in general, caching a reference to the compiled function is also a good performance boost.
-                    cache[tmpl] = {
-                        raw: str,
-                        template: HB.compile(str)
-                    };
-                }
-                callback(cache[tmpl]);
-            };
-        if (!cache[tmpl] || bypassCache) {
-            Y.log('Loading template from server: ' + tmpl, 'mojito', 'qeperf');
-            Y.io(tmpl, {
-                on: {
-                    complete: handler
-                }
-            });
-        } else {
-            callback(cache[tmpl]);
-        }
-    }
-
-    /**
      * Class text.
      * @class HandleBarsAdapterServer
      * @private
@@ -80,11 +47,56 @@ YUI.add('mojito-hb', function(Y, NAME) {
                 adapter.done('', meta);
             };
 
-            getTemplateObj(tmpl, !meta.view.cacheTemplates, handler);
+            this._getTemplateObj(tmpl, !meta.view.cacheTemplates, handler);
+        },
+
+        /**
+         * Cache the reference to a compiled handlebar template, plus
+         * a raw string representation of the template.
+         * @private
+         * @param {string} tmpl The name of the template to render.
+         * @param {boolean} bypassCache Whether or not we should rely on the cached content.
+         * @param {function} callback The function that is called with the compiled template
+         * @return {object} literal object with the "raw" and "template" references.
+         */
+        _getTemplateObj: function (tmpl, bypassCache, callback) {
+            var handler = function (templateStr) {
+                if (templateStr) {
+                    // applying a very simple local cache to avoid reading from disk over and over again.
+                    // in general, caching a reference to the compiled function is also a good performance boost.
+                    cache[tmpl] = {
+                        raw: templateStr,
+                        template: HB.compile(templateStr)
+                    };
+                }
+                callback(cache[tmpl]);
+            };
+            if (!cache[tmpl] || bypassCache) {
+                this._loadTemplate(tmpl, handler);
+            } else {
+                callback(cache[tmpl]);
+            }
+        },
+
+        /**
+         * Loads a template from a remote location
+         * @param tmpl The location of the template
+         * @param cb The callback to call with the template response
+         * @private
+         */
+        _loadTemplate: function (tmpl, cb) {
+            Y.log('Loading template from server: ' + tmpl, 'mojito', 'qeperf');
+            Y.io(tmpl, {
+                on: {
+                    complete: function (id, resp) {
+                        cb(resp.responseText);
+                    }
+                }
+            });
         }
     };
 
-    Y.mojito.addons.viewEngines.hb = HandleBarsAdapter;
+    Y.namespace('mojito.addons.viewEngines').hb = HandleBarsAdapter;
 
 }, '0.1.0', {requires: [
     'io-base',
--- source/lib/app/addons/view-engines/hb.server.js
+++ source/lib/app/addons/view-engines/hb.server.js
@@ -18,32 +18,6 @@ YUI.add('mojito-hb', function(Y, NAME) {
         cache = YUI.namespace('Env.Handlebars');
 
     /**
-     * Cache the reference to a compiled handlebar template, plus
-     * a raw string representation of the template.
-     * @param {string} tmpl The name of the template to render.
-     * @param {boolean} bypassCache Whether or not we should rely on the cached content.
-     * @return {object} literal object with the "raw" and "template" references.
-     */
-    function getTemplateObj(tmpl, bypassCache) {
-        var str;
-        if (!cache[tmpl] || bypassCache) {
-            Y.log('Loading template from disk: ' + tmpl, 'mojito', 'qeperf');
-            // TODO: should we try/catch this? I don't see any try/catch when reading files
-            //       in the rest of the repo, is this the intention?
-            str = fs.readFileSync(tmpl, 'utf8');
-            if (str) {
-                // applying a very simple local cache to avoid reading from disk over and over again.
-                // in general, caching a reference to the compiled function is also a good performance boost.
-                cache[tmpl] = {
-                    raw: str,
-                    template: HB.compile(str)
-                };
-            }
-        }
-        return cache[tmpl];
-    }
-
-    /**
      * Class text.
      * @class HandleBarsAdapterServer
      * @private
@@ -64,7 +38,7 @@ YUI.add('mojito-hb', function(Y, NAME) {
          * @param {boolean} more Whether there will be more content later.
          */
         render: function (data, mojitType, tmpl, adapter, meta, more) {
-            var obj = getTemplateObj(tmpl, !meta.view.cacheTemplates);
+            var obj = this._getTemplateObj(tmpl, !meta.view.cacheTemplates);
 
             if (obj) {
                 adapter.flush(obj.template(data), meta);
@@ -85,7 +59,7 @@ YUI.add('mojito-hb', function(Y, NAME) {
          * that can be sent to the client side.
          */
         compiler: function (tmpl) {
-            var obj = getTemplateObj(tmpl);
+            var obj = this._getTemplateObj(tmpl);
             return obj ? JSON.stringify(obj.str) : false;
         },
 
@@ -95,8 +69,45 @@ YUI.add('mojito-hb', function(Y, NAME) {
          * @return {string} the precompiled template that can be sent to the client side as Javascript code.
          */
         precompile: function (tmpl) {
-            var obj = getTemplateObj(tmpl);
+            var obj = this._getTemplateObj(tmpl);
             return obj ? HB.precompile(obj.str) : false;
+        },
+
+        /**
+         * Cache the reference to a compiled handlebar template, plus
+         * a raw string representation of the template.
+         * @private
+         * @param {string} tmpl The name of the template to render.
+         * @param {boolean} bypassCache Whether or not we should rely on the cached content.
+         * @return {object} literal object with the "raw" and "template" references.
+         */
+        _getTemplateObj: function (tmpl, bypassCache) {
+            var str;
+            if (!cache[tmpl] || bypassCache) {
+                Y.log('Loading template from disk: ' + tmpl, 'mojito', 'qeperf');
+                // TODO: should we try/catch this? I don't see any try/catch when reading files
+                //       in the rest of the repo, is this the intention?
+                str = this._loadTemplate(tmpl);
+                if (str) {
+                    // applying a very simple local cache to avoid reading from disk over and over again.
+                    // in general, caching a reference to the compiled function is also a good performance boost.
+                    cache[tmpl] = {
+                        raw: str,
+                        template: HB.compile(str)
+                    };
+                }
+            }
+            return cache[tmpl];
+        },
+
+        /**
+         * Loads a template from the file system
+         * @param tmpl The location of the template file
+         * @return {string} The template string
+         * @private
+         */
+        _loadTemplate: function (tmpl) {
+            return fs.readFileSync(tmpl, 'utf8');
         }
     };
 
