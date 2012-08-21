a3379352a732cfe4a17bdde9a33b276849250b57
mojit0 <idearat@yahoo-inc.com>
2012-08-21 17:27:43 -0600

    New files for server startup.

---
+++ server.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2011-2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
+var Mojito = require('./lib/mojito');
+
+Mojito.createServer();
