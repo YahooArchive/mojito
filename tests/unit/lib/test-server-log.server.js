00bd7973071cec956c2093375acbed8b6ef303ef
imalberto <imalberto@gmail.com>
2012-10-30 11:59:36 -0700

    FIX: upgraded arrow to 0.0.64 as recommended by @proverma; fix lib_test;

--- tests/unit/lib/test-server-log.server.js
+++ tests/unit/lib/test-server-log.server.js
@@ -7,6 +7,18 @@ YUI().use('test', function(Y) {
 
     var path = require('path'),
         suite = new Y.Test.Suite('mojito-server-log-tests'),
-        serverLog = require(path.join(__dirname, '../../../lib/server-log'));
+        serverLog = require(path.join(__dirname, '../../../lib/server-log')),
+        suite = new Y.Test.Suite('server-log-tests');
+
+    suite.add(new Y.Test.Case({
+
+    	name: 'server-log.server tests',
+
+    	'test serverLog not undefined': function() {
+    		Y.Test.Assert.isNotUndefined(serverLog, 'serverLog was not required correctly!');
+    	}
+    }));
+
+    Y.Test.Runner.add(suite);
 
 });
