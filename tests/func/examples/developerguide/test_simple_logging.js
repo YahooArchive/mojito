af14351e5cd7aaea5d7b002c1ffe9d0b76a77d93
Lichun Zhan <lzhan@yahoo-inc.com>
2012-12-03 15:23:23 -0800

    update test as the app is updated

--- tests/func/examples/developerguide/test_simple_logging.js
+++ tests/func/examples/developerguide/test_simple_logging.js
@@ -16,8 +16,10 @@ YUI({
                  Y.Assert.areEqual(" Server Configuration ", Y.all('h3').item(0).get('innerHTML'));
                  Y.Assert.areEqual(" Client Configuration ", Y.all('h3').item(1).get('innerHTML'));
                  Y.log("here....."+Y.all('div').item(2).get('innerHTML'));
-                 Y.Assert.areEqual("<b>Log level: </b> DEBUG <br>", Y.all('div').item(2).get('innerHTML').match(/<b>Log level: <\/b> DEBUG <br>/gi));
-                 Y.Assert.areEqual("<b>Log level: </b> INFO <br>", Y.all('div').item(2).get('innerHTML').match(/<b>Log level: <\/b> INFO <br>/gi));
+                 Y.Assert.areEqual("<b>Log level: </b>", Y.all('div').item(2).get('innerHTML').match(/<b>Log level: <\/b>/gi));
+                 Y.Assert.areEqual("debug", Y.all('div').item(2).get('innerHTML').match(/debug/gi));
+                 Y.Assert.areEqual("<b>Log level: </b>", Y.all('div').item(3).get('innerHTML').match(/<b>Log level: <\/b>/gi));
+                 Y.Assert.areEqual("info", Y.all('div').item(3).get('innerHTML').match(/info/gi));
              }
          }));    
 
