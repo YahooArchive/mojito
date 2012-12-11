c9a3c22a56f3ca5c61bda85adf5a4906ae45a8dd
Caridy Patino <caridy@gmail.com>
2012-12-11 18:36:30 -0500

    avoid warning when urlParams is empty/null. fixing functional tests for routing.

--- tests/func/applications/frameworkapp/routing/mojits/ShowUrl/controller.server.js
+++ tests/func/applications/frameworkapp/routing/mojits/ShowUrl/controller.server.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2011 Yahoo! Inc. All rights reserved.
  */
-YUI.add('ShowUrl', function(Y, NAME) {
+YUI.add('ShowUrl', function (Y, NAME) {
 
 /**
  * The ShowUrl module.
@@ -23,16 +23,19 @@ YUI.add('ShowUrl', function(Y, NAME) {
          * @param ac {Object} The action context that provides access
          *        to the Mojito API.
          */
-        index: function(ac) {
+        index: function (ac) {
             var mojitName = ac.params.getFromUrl('mojit_name'),
                 mojitAction = ac.params.getFromUrl('mojit_action'),
                 mojitUrlParams = ac.params.getFromUrl('mojit_urlparams'),
                 mojitUrl = ac.params.getFromUrl('mojit_url'),
-                mojitVerb = ac.params.getFromUrl('mojit_verb'),
+                mojitVerb = (ac.params.getFromUrl('mojit_verb') || 'get').toLowerCase(),
                 nameExists = mojitName ? "YES" : "NO",
-                actionExists = mojitAction ? "YES" : "NO";
-                urlparamsExists = mojitUrlParams ? "YES" : "NO";
-                urlExists = mojitUrl ? "YES" : "NO";
+                actionExists = mojitAction ? "YES" : "NO",
+                urlparamsExists = mojitUrlParams ? "YES" : "NO",
+                urlExists = mojitUrl ? "YES" : "NO",
+                url,
+                data,
+                matchroute;
 
             console.log("**********************" + mojitName);
             console.log("**********************" + mojitAction);
@@ -40,50 +43,51 @@ YUI.add('ShowUrl', function(Y, NAME) {
             console.log("**********************" + mojitUrl);
             console.log("**********************" + mojitVerb);
 
-            if (nameExists === "YES" && actionExists === "YES")
-            {
-                var url = "";
-                var error = "";
-                try{
-                    if (urlparamsExists === "YES"){
-                        url = ac.url.make(mojitName, mojitAction, 'MySpecialRoute', 'GET', mojitUrlParams);
+            if (nameExists === "YES" && actionExists === "YES") {
+                url = "";
+                try {
+                    if (urlparamsExists === "YES") {
+                        url = ac.url.make(mojitName, mojitAction, {
+                            id: 'MySpecialRoute'
+                        }, mojitVerb, {
+                            u: mojitUrlParams
+                        });
                     } else {
                         url = ac.url.make(mojitName, mojitAction);
                     }
-                }catch(error) {
-                    url = error;
+                } catch (err1) {
+                    url = err1;
                 }
-                var data = {
+                data = {
                     url: url,
                     name: mojitName,
                     action: mojitAction
                 };
                 //ac.http.setHeader('content-type', 'text/html');
                 ac.done(data);
-             } else if (urlExists === "YES"){
-                var matchroute = "";
-                var error = "";
-                try{
+            } else if (urlExists === "YES") {
+                matchroute = "";
+                try {
                     matchroute = ac.url.find(mojitUrl, mojitVerb);
-                }catch(error) {
-                    url = error;
+                } catch (err2) {
+                    url = err2;
                 }
                 console.log(matchroute);
-                if(matchroute != null){
-                    var data = {
+                if (matchroute !== null) {
+                    data = {
                         url: mojitUrl,
                         verbs: matchroute.verbs.GET,
                         call: matchroute.call,
                         name: matchroute.name,
-                        params: matchroute.params.secret,
-                    }
-                }else{
-                    var data = {
-                        url: mojitUrl,
-                    } 
+                        params: matchroute.params.secret
+                    };
+                } else {
+                    data = {
+                        url: mojitUrl
+                    };
                 }
                 ac.done(data);
-             }
+            }
         }
     };
 
