3d6febcd3f0320ad8fa294e735e506ce850fbf20
Isao Yagi <isao@yahoo-inc.com>
2013-01-07 15:02:31 -0800

    rename/move and tweak travis/script.sh

---
+++ tests/runall.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+# -e exits on any error; exit code is preserved
+
+# get abs path to mojito base dir
+basedir=$(cd $(dirname $0)/../ && pwd)
+
+$basedir/bin/mojito jslint -p
+$basedir/tests/run.js test -u --group server --driver nodejs
+$basedir/tests/run.js test -u --group client --driver selenium
+$basedir/tests/run.js test -f
