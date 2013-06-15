da7a1fa43f3946abe29b40b3adc7306395b72725
Joe Catera <zhouyaoji@yahoo.com>
2013-06-14 19:05:32 -0700

    Fixed model test.

--- examples/dashboard/06_testing/mojits/Github/tests/models/yql.server-tests.js
+++ examples/dashboard/06_testing/mojits/Github/tests/models/yql.server-tests.js
@@ -7,36 +7,31 @@ YUI.add('StatsModelYQL-tests', function(Y, NAME) {
     suite.add(new YUITest.TestCase({
         
         name: 'StatsModelYQL user tests',
-        
         setUp: function() {
             model = Y.mojito.models.StatsModelYQL;
         },
         tearDown: function() {
             model = null;
         },
-        
         'test mojit model': function() {
             var called = false,
                 cfg = { color: 'red' };
 
             A.isNotNull(model);
-
             A.isFunction(model.init);
             model.init(cfg);
             A.areSame(cfg, model.config);
-
             A.isFunction(model.getData);
-        
-            model.getData(function(err, data) {
+            model.getData({}, function(cb, data) {
                 called = true;
-                Y.log(err, "info", NAME);
-                A.isTrue(!err);
-                A.isObject(data);
-                A.areSame('data', data.some);
+                A.isFunction(cb);
+                return { 
+                      cb(callback, result){
+                         callback(result);
+                };
             });
+            A.isTrue(called);
         }
     }));
-    
     YUITest.TestRunner.add(suite);
-    
 }, '0.0.1', {requires: ['mojito-test', 'StatsModelYQL']});
