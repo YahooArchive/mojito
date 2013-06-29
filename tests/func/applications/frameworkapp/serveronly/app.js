1803043cc0dd29bd82da795549d17867b82484dd
imalberto <imalberto@gmail.com>
2013-06-29 00:03:36 -0700

    Fix fund tests : - tests/func/serveronly

--- tests/func/applications/frameworkapp/serveronly/app.js
+++ tests/func/applications/frameworkapp/serveronly/app.js
@@ -20,6 +20,22 @@ app.use(mojito.middleware());
 app.mojito.attachRoutes();
 app.post('/tunnel', mojito.tunnelMiddleware());
 
+/*
+    "default": {
+        "verbs": ["get", "post", "put", "head", "delete"],
+        "path": "/:mojit-base/:mojit-action",
+        "call": "{mojit-base}.{mojit-action}"
+    }
+*/
+function rt(req, res, next) {
+    mojito.dispatch(req.params.mojitBase + '.' + req.params.mojitAction)(req, res, next);
+}
+app.get('/:mojitBase/:mojitAction', rt);
+app.post('/:mojitBase/:mojitAction', rt);
+app.put('/:mojitBase/:mojitAction', rt);
+app.head('/:mojitBase/:mojitAction', rt);
+app.delete('/:mojitBase/:mojitAction', rt);
+
 app.get('/status', function (req, res) {
     res.send('200 OK');
 });
