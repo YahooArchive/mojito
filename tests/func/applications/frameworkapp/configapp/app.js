757600bd4350221bd15f36db2c5f1775c49317f1
imalberto <imalberto@gmail.com>
2013-07-01 17:37:19 -0700

    Linted

--- tests/func/applications/frameworkapp/configapp/app.js
+++ tests/func/applications/frameworkapp/configapp/app.js
@@ -23,11 +23,11 @@ app.post('/tunnel', mojito.tunnelMiddleware());
 function rt(req, res, next) {
     return mojito.dispatch(req.params.type + '.' + req.params.action)(req, res, next);
 }
-app.get('/:type/:action', rt)
-app.post('/:type/:action', rt)
-app.put('/:type/:action', rt)
-app.head('/:type/:action', rt)
-app.delete('/:type/:action', rt)
+app.get('/:type/:action', rt);
+app.post('/:type/:action', rt);
+app.put('/:type/:action', rt);
+app.head('/:type/:action', rt);
+app['delete']('/:type/:action', rt);
 
 app.get('/status', function (req, res) {
     res.send('200 OK');
