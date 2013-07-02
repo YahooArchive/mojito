4f4298d78cee0814e66f7f55d100833d5cb99ec1
imalberto <imalberto@gmail.com>
2013-07-01 17:14:44 -0700

    Restored some of the functional tests under tests/func/common/*

--- tests/func/applications/frameworkapp/common/app.js
+++ tests/func/applications/frameworkapp/common/app.js
@@ -21,6 +21,58 @@ app.use(mojito.middleware());
 app.mojito.attachRoutes();
 app.post('/tunnel', mojito.tunnelMiddleware());
 
+// "routeparamssimple": {
+//     "verbs": ["get"],
+//     "path": "/RouteParamsSimple",
+//     "call": "RouteParams.routeParamsSimple",
+//     "params": "foo=fooval&bar=barval"
+// },
+app.get('/RouteParamsSimple', function (req, res, next) {
+    req.params = req.params || {};
+    req.params.foo = 'fooval';
+    req.params.bar = 'barval';
+}, mojito.dispatch('RouteParams.routeParamsSimple'));
+
+// "mergeparamssimple": {
+//     "verbs": ["post"],
+//     "path": "/MergeParamsSimple",
+//     "call": "MergeParams.mergeParamsSimple",
+//     "params": {
+//         "likes": "Beer"
+//     }
+// }
+app.post('/MergeParamsSimple', function (req, res, next) {
+    req.params = req.params || {};
+    req.params.likes = 'Beer';
+}, mojito.dispatch('MergeParams.mergeParamsSimple'));
+
+
+// "mergeparams": {
+//     "verbs": ["post"],
+//     "path": "/MergeParams",
+//     "call": "MergeParams.mergeParams",
+//     "params": "likes=Beer"
+//  },
+app.post('/MergeParams', function (req, res, next) {
+    req.params = req.params || {};
+    req.params.likes = 'Beer';
+}, mojito.dispatch('MergeParams.mergeParams'));
+
+// "routeparams": {
+//     "verbs": ["get"],
+//     "path": "/RouteParams",
+//     "call": "RouteParams.routeParams",
+//     "params": {
+//        "foo": "fooval",
+//        "bar": "barval"
+//     }
+// },
+app.get('/RouteParams', function (req, res, next) {
+    req.params = req.params || {};
+    req.params.foo = 'fooval';
+    req.params.bar = 'barval';
+}, mojito.dispatch('RouteParams.routeParams'));
+
 // "default": {
 //     "verbs": ["get", "post", "put", "head", "delete"],
 //     "path": "/:mojit-id/:mojit-action",
@@ -33,7 +85,7 @@ app.get('/:mojitType/:mojitAction', rt);
 app.post('/:mojitType/:mojitAction', rt);
 app.put('/:mojitType/:mojitAction', rt);
 app.head('/:mojitType/:mojitAction', rt);
-app.delete('/:mojitType/:mojitAction', rt);
+app['delete']('/:mojitType/:mojitAction', rt);
 
 app.get('/status', function (req, res) {
     res.send('200 OK');
