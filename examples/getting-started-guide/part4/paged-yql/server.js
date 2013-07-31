caa170fa863f506c0112581edc33efd92800f2fc
Lichun Zhan <lzhan@yahoo-inc.com>
2013-07-31 14:40:19 -0700

    rm server.js file

--- examples/getting-started-guide/part4/paged-yql/server.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2011-2013, Yahoo! Inc.  All rights reserved.
- * Copyrights licensed under the New BSD License.
- * See the accompanying LICENSE file for terms.
- */
-/*jslint nomen: true */
-
-module.exports = require('mojito').createServer({dir: __dirname});
