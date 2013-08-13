740f5a3c7cd4dc7a66644589e330ff309f7ad819
Joe Catera <zhouyaoji@yahoo.com>
2013-08-13 14:00:53 -0700

    Fixed more conflicts.

--- tests/func/examples/developerguide/test_03_frame_mojits.js
+++ tests/func/examples/developerguide/test_03_frame_mojits.js
@@ -5,16 +5,18 @@
       }).use('node', 'node-event-simulate', 'test', 'console', function (Y) {
 
         'use strict';
-        var suite = new Y.Test.Suite("Dashboard App: Frame Mojit Module"),
+        var suite = new Y.Test.Suite("TribApp: Mojito Dashboard test"),
             url = window.location.protocol + "//" + window.location.host + "/";
         suite.add(new Y.Test.Case({
-          "test HTML title and heading for GitHub mojit": function () {
+          "test Mojito dashboard": function () {
             // Tests the title in HTML header
             Y.Assert.areEqual("Trib - YUI/Mojito Developer Dashboard", Y.one('head title').get('innerHTML'));
 
+            // Tests the  YUI button
+            Y.Assert.areEqual(url, Y.one('a.yui3-button.swap').get('href'));
 
             // Tests the title within the content
-            Y.Assert.areEqual("YUI GitHub Stats", Y.one('div h3').get('innerHTML'));
+            Y.Assert.areEqual("Trib - Mojito Developer Dashboard", Y.one('body h1').get('innerHTML'));
           }
         }));
         Y.Test.Runner.add(suite);
--- tests/func/examples/developerguide/test_05_getting_data.js
+++ tests/func/examples/developerguide/test_05_getting_data.js
@@ -5,16 +5,18 @@
       }).use('node', 'node-event-simulate', 'test', 'console', function (Y) {
 
         'use strict';
-        var suite = new Y.Test.Suite("Dashboard App: Getting Data Module"),
+        var suite = new Y.Test.Suite("TribApp: Mojito Dashboard test"),
             url = window.location.protocol + "//" + window.location.host + "/";
         suite.add(new Y.Test.Case({
-          "test HTML title and heading of page": function () {
+          "test Mojito dashboard": function () {
             // Tests the title in HTML header
             Y.Assert.areEqual("Trib - YUI/Mojito Developer Dashboard", Y.one('head title').get('innerHTML'));
 
+            // Tests the  YUI button
+            Y.Assert.areEqual(url, Y.one('a.yui3-button.swap').get('href'));
 
             // Tests the title within the content
-            Y.Assert.areEqual("Trib - Contribute to the Tribe", Y.one('body h1').get('innerHTML'));
+            Y.Assert.areEqual("Trib - Mojito Developer Dashboard", Y.one('body h1').get('innerHTML'));
           }
         }));
         Y.Test.Runner.add(suite);
