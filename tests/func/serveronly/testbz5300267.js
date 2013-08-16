1678c2d1bba91e4d3981919a14e91bf06d77f723
Lichun Zhan <lzhan@yahoo-inc.com>
2013-08-15 17:42:52 -0700

    fix test with 0 passed 0 failed result

--- tests/func/serveronly/testbz5300267.js
+++ tests/func/serveronly/testbz5300267.js
@@ -1,11 +1,7 @@
 /*
  * This is a basic func test for a Serveronly application.
  */
-YUI({
-    useConsoleOutput: true,
-    useBrowserConsole: true,
-    logInclude: { TestRunner: true }
-}).use('node', 'node-event-simulate', 'test', 'console', function (Y) {
+YUI.add('serveronly-bz5300267-tests', function (Y) {
    
      var suite = new Y.Test.Suite("ServerOnly: bz5300267");
 
@@ -19,4 +15,6 @@ YUI({
   }));
 
   Y.Test.Runner.add(suite);
-});
+}, '0.0.1', {requires: [
+  'node', 'node-event-simulate', 'test'
+]});
--- tests/func/serveronly/testbz5366776.js
+++ tests/func/serveronly/testbz5366776.js
@@ -1,11 +1,7 @@
 /*
  * This is a basic func test for a Serveronly application.
  */
-YUI({
-    useConsoleOutput: true,
-    useBrowserConsole: true,
-    logInclude: { TestRunner: true }
-}).use('node', 'node-event-simulate', 'test', 'console', function (Y) {
+YUI.add('serveronly-bz366776-tests', function (Y) {
    
      var suite = new Y.Test.Suite("ServerOnly: bz5366776");
 
@@ -17,4 +13,6 @@ YUI({
   }));
 
   Y.Test.Runner.add(suite);
-});
+}, '0.0.1', {requires: [
+  'node', 'node-event-simulate', 'test'
+]});
