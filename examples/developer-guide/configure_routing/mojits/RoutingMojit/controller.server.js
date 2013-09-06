b34aa621264855bd7ca742eede392e55cd91cfc9
imalberto <imalberto@gmail.com>
2013-09-05 18:27:49 -0700

    More tests fixes.

--- examples/developer-guide/configure_routing/mojits/RoutingMojit/controller.server.js
+++ examples/developer-guide/configure_routing/mojits/RoutingMojit/controller.server.js
@@ -15,21 +15,19 @@ YUI.add('RoutingMojit', function(Y, NAME) {
             path = ac.http.getRequest().url,
             routes = ac.config.getRoutes();
 
-		// Mojito no longer support more than one VERB per route entry
-		// The route.name is now the route.path + '.' + route.call
         if (action === "index" && path === "/") {
             name = "root_route";
-			if (routes['/.mapped_mojit.index'].verbs['GET']) {
+            if (routes[name].methods.get) {
                 methods = 'GET';
             }
-        } else if (action==="index") {
+        } else if (action === "index") {
             name = "index_route";
-			if (routes['/index.mapped_mojit.index'].verbs['GET']) {
+			if (routes[name].methods.get) {
                 methods = 'GET';
             }
         } else {
             name = "show_route";
-			if (routes['/show.mapped_mojit.show'].verbs['POST']) {
+			if (routes[name].methods.post) {
                 methods = 'POST';
             }
         }
