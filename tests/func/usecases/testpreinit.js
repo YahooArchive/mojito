2a19b76d1a6c3583005920a708012528c1bdb548
Lichun Zhan <lzhan@yahoo-inc.com>
2013-10-10 13:07:04 -0700

    update usecase app and tests using contextualized model

--- tests/func/usecases/testpreinit.js
+++ tests/func/usecases/testpreinit.js
@@ -10,7 +10,7 @@ YUI.add('usecases-testpreinit-tests', function (Y) {
             Y.Assert.areEqual('Enjoy your Flickr Images!',Y.one('h2').get('innerHTML').match(/Enjoy your Flickr Images!/gi));
             var imagelink = Y.all('a').item(1).get('href');
             Y.Assert.areEqual('http:',imagelink.match(/http:/gi));
-            Y.Assert.areEqual('static.flickr.com',imagelink.match(/static.flickr.com/gi));
+            Y.Assert.areEqual('/static/usecase/assets',imagelink.match(/\/static\/usecase\/assets/gi));
             Y.Assert.areEqual('page=2', Y.one('#paginate a').get('href').match(/page=2/gi));
             Y.Assert.areEqual('next', Y.one('#paginate a').get('innerHTML'));
             Y.one('#paginate a').simulate('click');
