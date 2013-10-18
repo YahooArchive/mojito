813a866d43b8cf984c1e5153a2804c85e4507af2
Joe Catera <zhouyaoji@yahoo.com>
2013-10-18 16:13:10 -0700

    Changed model name from 'foo' to 'model'.

--- examples/developer-guide/dashboard/01_mojito_cli_basics/mojits/Github/controller.server.js
+++ examples/developer-guide/dashboard/01_mojito_cli_basics/mojits/Github/controller.server.js
@@ -22,7 +22,7 @@ YUI.add('Github', function(Y, NAME) {
          *        to the Mojito API.
          */
         index: function(ac) {
-            ac.models.get('foo').getData(function(err, data) {
+            ac.models.get('model').getData(function(err, data) {
                 if (err) {
                     ac.error(err);
                     return;
