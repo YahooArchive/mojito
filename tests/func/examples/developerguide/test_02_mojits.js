62b793399ddd112566882e7ea454b966b6b9f678
Joe Catera <zhouyaoji@yahoo.com>
2013-11-12 15:57:59 -0800

    Updated test files and some of the controllers.

--- tests/func/examples/developerguide/test_02_mojits.js
+++ tests/func/examples/developerguide/test_02_mojits.js
@@ -5,17 +5,6 @@
       }).use('node', 'node-event-simulate', 'test', 'console', function (Y) {
 
         'use strict';
-<<<<<<< HEAD
-        var suite = new Y.Test.Suite("Dashboard App: Mojits Module"),
-            url = window.location.protocol + "//" + window.location.host + "/";
-        suite.add(new Y.Test.Case({
-          "test heading of page and GitHub section": function () {
-            // Tests the title in HTML header
-            Y.Assert.areEqual("Trib - YUI/Mojito Developer Dashboard", Y.one('div h4').get('innerHTML'));
-
-            // Tests the title within the content
-            Y.Assert.areEqual("YUI GitHub Stats", Y.one('div h3').get('innerHTML'));
-=======
         var suite = new Y.Test.Suite("TribApp: Mojito Dashboard test"),
             url = window.location.protocol + "//" + window.location.host + "/";
         suite.add(new Y.Test.Case({
@@ -28,7 +17,6 @@
 
             // Tests the title within the content
             Y.Assert.areEqual("Trib - Mojito Developer Dashboard", Y.one('body h1').get('innerHTML'));
->>>>>>> 9d15dfb... Added descriptor for dashboard module apps and templates for the test files.
           }
         }));
         Y.Test.Runner.add(suite);
--- tests/func/examples/developerguide/test_07_binders.js
+++ tests/func/examples/developerguide/test_07_binders.js
@@ -5,17 +5,6 @@
       }).use('node', 'node-event-simulate', 'test', 'console', function (Y) {
 
         'use strict';
-<<<<<<< HEAD
-        var suite = new Y.Test.Suite("Dashboard App: Binders Module"),
-            url = window.location.protocol + "//" + window.location.host + "/";
-        suite.add(new Y.Test.Case({
-          "test HTML title and Blog mojit heading": function () {
-            // Tests the title in HTML header
-            Y.Assert.areEqual("Trib - YUI/Mojito Developer Dashboard", Y.one('head title').get('innerHTML'));
-
-            // Tests the title within the content
-            Y.Assert.areEqual("YUI Blog Posts", Y.one('#blog h3 strong').get('innerHTML'));
-=======
         var suite = new Y.Test.Suite("TribApp: Mojito Dashboard test"),
             url = window.location.protocol + "//" + window.location.host + "/";
         suite.add(new Y.Test.Case({
@@ -28,7 +17,6 @@
 
             // Tests the title within the content
             Y.Assert.areEqual("Trib - Mojito Developer Dashboard", Y.one('body h1').get('innerHTML'));
->>>>>>> 9d15dfb... Added descriptor for dashboard module apps and templates for the test files.
           }
         }));
         Y.Test.Runner.add(suite);
