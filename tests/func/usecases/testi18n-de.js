154f13820abfaf100af9983b1e6f813d5baff3b0
Lichun Zhan <lzhan@yahoo-inc.com>
2014-02-04 09:45:58 -0800

    update tests to make time/date related tests run fine on both grid and local machines

--- tests/func/usecases/testi18n-de.js
+++ tests/func/usecases/testi18n-de.js
@@ -17,9 +17,16 @@ YUI.add('usecases-testi18n-de-tests', function (Y) {
                 "x":"%d/%m/%Y"
             });
             Y.Intl.setLang("datatype-date-format", "de");
+            var date = new Date();
+                UTCDate = new Date();
+            UTCDate.setTime(date.getTime()+date.getTimezoneOffset()*60000)
             var mydate = Y.DataType.Date.format(new Date(), {format:"%d/%m/%Y"});
-            var expecteddate = mydate.slice(0,6)+mydate.slice(8,10);
-            Y.Assert.areEqual(expecteddate.replace(/\//g, "."), title.substr(title.indexOf('-')+2, 8));
+                myUTCDate = Y.DataType.Date.format(UTCDate, {format:"%d/%m/%Y"});
+            var expecteddate1 = mydate.slice(0,6)+mydate.slice(8,10);
+                expecteddate2 = myUTCDate.slice(0,6)+mydate.slice(8,10);
+                actualdate = title.substr(title.indexOf('-')+2, 8);
+            Y.Assert.isTrue(expecteddate1.replace(/\//g, ".") === actualdate 
+                || expecteddate2.replace(/\//g, ".") == actualdate);
         }
 
     }));
