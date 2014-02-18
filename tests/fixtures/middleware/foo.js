38aaa8edf8e283739b112c328b84c6e0ab1aa2ac
imalberto <imalberto@gmail.com>
2014-02-18 10:24:06 -0800

    Remove debug logs from middleware fixtures.

--- tests/fixtures/middleware/foo.js
+++ tests/fixtures/middleware/foo.js
@@ -1,5 +1,4 @@
 
 module.exports = function (req, res, next) {
-    console.log('--------- foo exec');
     next();
 };
--- tests/fixtures/middleware/mojito-foo.js
+++ tests/fixtures/middleware/mojito-foo.js
@@ -1,9 +1,7 @@
 
 module.exports = function (midConfig) {
-    console.log('------ mojito-foo init');
     return function (req, res, next) {
         res.midConfig = midConfig;
-        console.log('------ mojito-foo exec');
         next();
     };
 };
